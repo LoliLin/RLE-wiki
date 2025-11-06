75a2d5b162adbf8d34e5cdfe4f72b19334be48af
洛铃 <teawhat2000@gmail.com>
2025-11-06 14:00:44 +0800

    Update links to RLE-wiki paths in config
    Signed-off-by: 洛铃 <teawhat2000@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,42 +9,42 @@ type NavConfig = DefaultTheme.Config['nav']
 const nav: NavConfig = [
   {
     text: "大学指南",
-    link: "/campus/",
+    link: "/RLE-wiki/campus/",
   },
   {
     text: "时尚护理",
-    link: "/fashion/",
+    link: "/RLE-wiki/fashion/",
   },
   {
     text: "安全防护",
-    link: "/personal-safety/",
+    link: "/RLE-wiki/personal-safety/",
   },
   {
     text: "志愿填报",
-    link: "/admission/",
+    link: "/RLE-wiki/admission/",
   },
   {
     text: "海外生活",
-    link: "/overseas/",
+    link: "/RLE-wiki/overseas/",
   },
   {
     text: "其它",
-    link: "/others/",
+    link: "/RLE-wiki/others/",
   },
   {
     text: "贡献指南",
     items: [
       {
         text: "校园版块投稿指南",
-        link: "/contributor-guide/campus.md",
+        link: "/RLE-wiki/contributor-guide/campus.md",
       },
       {
         text: "其他投稿指南",
-        link: "/contributor-guide/other.md",
+        link: "/RLE-wiki/contributor-guide/other.md",
       },
       {
         text: "校园版块贡献模板",
-        link: "/contributor-guide/CampusTemplate.md",
+        link: "/RLE-wiki/contributor-guide/CampusTemplate.md",
       },
     ],
   },
@@ -64,43 +64,43 @@ const sidebarOptions = [
   {
     ...baseConfig,
     scanStartPath: "campus",
-    resolvePath: "/campus/",
+    resolvePath: "/RLE-wiki/campus/",
   },
   // 贡献指南
   {
     ...baseConfig,
     scanStartPath: "contributor-guide",
-    resolvePath: "/contributor-guide/",
+    resolvePath: "/RLE-wiki/contributor-guide/",
   },
   // 时尚护理
   {
     ...baseConfig,
     scanStartPath: "fashion",
-    resolvePath: "/fashion/",
+    resolvePath: "/RLE-wiki/fashion/",
   },
   // 安全防护
   {
     ...baseConfig,
     scanStartPath: "personal-safety",
-    resolvePath: "/personal-safety/",
+    resolvePath: "/RLE-wiki/personal-safety/",
   },
   // 志愿填报
   {
     ...baseConfig,
     scanStartPath: "admission",
-    resolvePath: "/admission/",
+    resolvePath: "/RLE-wiki/admission/",
   },
   // 海外生活
   {
     ...baseConfig,
     scanStartPath: 'overseas',
-    resolvePath: '/overseas/',
+    resolvePath: '/RLE-wiki/overseas/',
   },
   // 其它
   {
     ...baseConfig,
     scanStartPath: "others",
-    resolvePath: "/others/",
+    resolvePath: "/RLE-wiki/others/",
   },
 ];
 const disclaimerStr = '<p>特别注意，RLE.wiki 中的医学、法律或其他专业内容仅供参考，并不能替代专业人士的建议。RLE.wiki 不提供法律或医学建议，请在必要时咨询合资格的专家。</p>' +
@@ -118,7 +118,7 @@ const themeConfig: ThemeContext = {
   // SiteTitle值为false时，logo位置不显示标题。未定义SiteTitle时，显示标题。SiteTitle值为abcd时，显示abcd。
   SiteTitle: false,
   /** Repo */
-  githubRepoLink: 'https://github.com/project-trans/RLE-wiki',
+  githubRepoLink: 'https://github.com/LoliLin/RLE-wiki',
   /** vitepress 根目录 */
   rootDir: 'docs',
   /** 文档所在目录（目前似未使用此项） */
@@ -127,40 +127,40 @@ const themeConfig: ThemeContext = {
   sidebarOptions,
   /** 文档所在目录（用于GitHub编辑链接） */
   sitePattern: `docs`,
-  hostName: 'https://rle.wiki',
+  hostName: 'https://lolilin.github.io/RLE-wiki',
   enableDisclaimer: true,
   disclaimerPaths: [
     {
-      path: '/campus/',
+      path: '/RLE-wiki/campus/',
       summaryHtml: 'RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。',
       detailHtml: '<p>RLE.wiki「大学指南」中的内容，仅供参考。可能存在过时或不准确的信息，请谨慎甄别。</p>' +
         '<p>「大学指南」板块中的内容，多数来自于读者投稿，并经编辑简单整理和形式审查后登载，主要体现投稿者主观观点，不代表 RLE.wiki 编辑团队及我们的任何相关维护人员立场。</p>' +
         disclaimerStr,
     },
     {
-      path: '/fashion/',
+      path: '/RLE-wiki/fashion/',
       summaryHtml: 'RLE.wiki「时尚护理」中的内容，多数来自于读者投稿，主要体现投稿者主观观点，仅供参考。',
       detailHtml: '<p>RLE.wiki「时尚护理」中的内容，多数来自于读者投稿，主要体现投稿者主观观点，仅供参考。</p>' +
         '<p>「时尚护理」板块中的内容，多数仅为对投稿内容进行简单整理和形式审查后登载，不代表 RLE.wiki 编辑团队及我们的任何相关维护人员立场。</p>' +
         disclaimerStr,
     },
     {
-      path: '/personal-safety/',
+      path: '/RLE-wiki/personal-safety/',
       summaryHtml: commonDisclaimerStrSummary,
       detailHtml: commonDisclaimerStrDetail,
     },
     {
-      path: '/admission/',
+      path: '/RLE-wiki/admission/',
       summaryHtml: commonDisclaimerStrSummary,
       detailHtml: commonDisclaimerStrDetail,
     },
     {
-      path: '/overseas/',
+      path: '/RLE-wiki/overseas/',
       summaryHtml: commonDisclaimerStrSummary,
       detailHtml: commonDisclaimerStrDetail,
     },
     {
-      path: '/others/',
+      path: '/RLE-wiki/others/',
       summaryHtml: commonDisclaimerStrSummary,
       detailHtml: commonDisclaimerStrDetail,
     },
